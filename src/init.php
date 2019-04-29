91b82fbedaa84f8d4e55e4b09e8391546edbae9d
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2019-04-29 13:14:16 -0700

    🐛 FIX: Cache Clearing and JS Load Order

--- src/init.php
+++ src/init.php
@@ -25,8 +25,8 @@ function bcfed_bc_final_exam_display_block_assets() {
 	wp_enqueue_style(
 		'bcfed_bc_final_exam_display-style-css', // Handle.
 		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
-		array( 'wp-blocks' ) // Dependency to include the CSS after it.
-		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: filemtime — Gets file modification time.
+		array( 'wp-blocks' ), // Dependency to include the CSS after it.
+		filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: filemtime — Gets file modification time.
 	);
 } // End function bcfed_bc_final_exam_display_block_assets().
 
@@ -47,8 +47,8 @@ function bcfed_bc_final_exam_display_editor_assets() {
 	wp_enqueue_script(
 		'bcfed_bc_final_exam_display-block-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element' ), // Dependencies, defined above.
-		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-components', 'wp-editor' ), // Dependencies, defined above.
+		filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
 		true // Enqueue the script in the footer.
 	);
 
@@ -56,8 +56,8 @@ function bcfed_bc_final_exam_display_editor_assets() {
 	wp_enqueue_style(
 		'bcfed_bc_final_exam_display-block-editor-css', // Handle.
 		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
-		array( 'wp-edit-blocks' ) // Dependency to include the CSS after it.
-		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: filemtime — Gets file modification time.
+		array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
+		filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: filemtime — Gets file modification time.
 	);
 } // End function bc_final_exam_cgb_editor_assets().
 
